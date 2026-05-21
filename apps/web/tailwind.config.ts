edb71dd79585b32989e837d164fdce1be14203e2
thiagomeireles <thiagomeireles098@gmail.com>
2026-05-21 12:54:04 -0300

    Update tailwind.config.ts

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,1 +1,7 @@
-// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+} satisfies Config;
