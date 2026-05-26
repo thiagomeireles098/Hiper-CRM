fb9b79980ec4ece615f7fccaaa09f6cfa2a2025d
vepifanio <vepifanio1@gmail.com>
2026-05-26 11:44:15 -0300

    feat(api): add health endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { WorkspacesModule } from "./workspaces/workspaces.module";
+import { HealthController } from "./health.controller";
 
 @Module({
-  imports: [PrismaModule, AuthModule, WorkspacesModule]
+  imports: [PrismaModule, AuthModule, WorkspacesModule],
+  controllers: [HealthController]
 })
 export class AppModule {}
